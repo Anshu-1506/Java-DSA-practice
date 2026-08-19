74fc6f19985f95acb7e4fbf21b31cb9492ae6438
Anshuman <anshumantiwari153@gmail.com>
2026-08-19 11:09:26 +0530

    solve merge two sorted arrays

--- DSA/Arrays/twoPointers.java
+++ DSA/Arrays/twoPointers.java
@@ -88,20 +88,56 @@ public class twoPointers {
     
 
      //Remove duplicate elements iarray from the sorted array
-    int[] arr = {1,1,2,3,3,4,5,5};
-    int i = 0;
-    
-    for(int j=1;j<arr.length;j++){
-        if(arr[i]!=arr[j]){
-            i++;
-            arr[i]=arr[j];
-        }
-        
-    }
-    int uniquecount = i+1;
 
-    for(int k=0;k<uniquecount;k++){
-        System.out.print(arr[k]+" ");
+    // int[] arr = {1,1,2,3,3,4,5,5};
+    // int i = 0;
+    
+    // for(int j=1;j<arr.length;j++){
+    //     if(arr[i]!=arr[j]){
+    //         i++;
+    //         arr[i]=arr[j];
+    //     }
+        
+    // }
+    // int uniquecount = i+1;
+
+    // for(int k=0;k<uniquecount;k++){
+    //     System.out.print(arr[k]+" ");
+    // }
+
+    //Merge two sorted arrays
+
+    int[] arr1={1,4,7,10};
+    int[] arr2={2,3,6,8,9};
+
+    int i=0;
+    int j=0;
+    int[] result = new int[arr1.length+arr2.length];
+    int k=0;
+
+    while(i<arr1.length && j<arr2.length){
+        if(arr1[i]<arr2[j]){
+            result[k]=arr1[i];
+            i++;
+        }
+        else{
+            result[k]=arr2[j];
+            j++;
+        }
+    k++;
+    }
+    while(i<arr1.length){
+        result[k]=arr1[i];
+        i++;
+        k++;
+    }
+    while(j<arr2.length){
+        result[k]=arr2[j];
+        j++;
+        k++;
+    }
+    for(int m=0;m<result.length;m++){
+        System.out.print(result[m]+" ");
     }
     }
     
