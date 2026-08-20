4c5053c8442730ec0ee2e3a686bbc797d55486ed
Anshuman <anshumantiwari153@gmail.com>
2026-08-20 12:40:53 +0530

    solve maximum sum of 3 consecutive elements

---
+++ DSA/Arrays/slidingWindow.java
@@ -0,0 +1,64 @@
+public class slidingWindow {
+    public static void main(String[] args){
+        
+        //Maximum sum of 3 consecutive elements
+        // int[] arr = {4,2,7,1,8,3};
+        // int k = 3;
+        // int windowSum=0;
+        // //first window
+        // for(int i=0;i<k;i++){
+        // windowSum+=arr[i];
+        // }
+        // int maxSum = windowSum;
+
+        // //slide the window
+        // for(int i=k;i<arr.length;i++){
+        //     windowSum=windowSum-arr[i-k]+arr[i];
+        //     if(windowSum>maxSum){
+        //         maxSum=windowSum;
+        //     }
+        // }
+        // System.out.println(maxSum);
+
+        //Minimum sum of 2 consecutive elements
+        // int[] arr = {5,2,8,1,6};
+        // int k = 2;
+        // int windowSum=0;
+        // //first window
+        // for(int i=0;i<k;i++){
+        // windowSum+=arr[i];
+        // }
+        // int minSum = windowSum;
+
+        // //slide the window
+        // for(int i=k;i<arr.length;i++){
+        //     windowSum=windowSum-arr[i-k]+arr[i];
+        //     if(windowSum<minSum){
+        //         minSum=windowSum;
+        //     }
+        // }
+        // System.out.println(minSum);
+
+        //Average of 3 consecutive elements
+
+        int [] arr = {2,4,6,8,10};
+        int k=3;
+        double windowAvg=0;
+        int windowSum=0;
+
+        //first window
+        for(int i =0;i<k;i++){
+        windowSum+=arr[i];
+        }
+        windowAvg=windowSum/k;
+        System.out.println(windowAvg);
+
+        //Slide the window
+        for(int i=k;i<arr.length;i++){
+        windowSum=windowSum-arr[i-k]+arr[i];
+         windowAvg=windowSum/k;
+        System.out.println(windowAvg);
+             }
+       
+    }
+}
