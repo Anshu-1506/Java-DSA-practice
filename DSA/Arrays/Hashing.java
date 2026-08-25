2e79bdb6d115757eef646e7dedaedc7de45a8800
Anshuman <anshumantiwari153@gmail.com>
2026-08-25 09:17:48 +0530

    find duplicated elements in an array

---
+++ DSA/Arrays/Hashing.java
@@ -0,0 +1,62 @@
+import java.util.HashMap;
+public class Hashing{
+    public static void main(String[] args) {
+        // int[] arr = {1,2,2,3,1,4,2};
+
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        
+        // for(int i=0;i<arr.length;i++){
+        //     if(map.containsKey(arr[i])){
+        //     map.put((arr[i]), map.get(arr[i])+1);
+        //     }
+        //     else{
+        //         map.put(arr[i],1);
+        //     }
+        // }
+        // System.out.println(map);
+
+        //Find duplicates elements in an array
+
+    //     int[] arr = {1,2,3,2,4,5,1,3};
+
+    //     HashMap<Integer, Integer> map = new HashMap<>();
+
+    //    // frequency
+    //     for(int i=0;i<arr.length;i++){
+    //         if(map.containsKey(arr[i])){
+    //             map.put((arr[i]), map.get(arr[i])+1);
+
+    //         }
+    //         else{
+    //             map.put(arr[i],1);
+    //         }
+    //     }
+    //     // find duplicates
+
+    //     for(int i=0;i<arr.length;i++){
+    //        if(map.get(arr[i])>1){
+    //         System.out.println(arr[i]);
+    //         map.put(arr[i], 1);
+    //        }
+    //     }
+    
+
+    //Find Frequency of Array Elements
+
+    // int[] arr = {4,5,4,2,5,5,1};
+
+    // HashMap<Integer, Integer>map = new HashMap<>();
+
+    // for(int i=0;i<arr.length;i++){
+    //     if(map.containsKey(arr[i])){
+    //         map.put(arr[i],map.get(arr[i])+1);
+    //     }
+    //     else{
+    //         map.put(arr[i], 1);
+    //     }
+    // }
+    // System.out.println(map);
+
+    
+    }
+}
