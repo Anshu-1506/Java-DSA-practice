e8c8728d0c813dc805ca50993fb3420e0aaa10c9
Anshuman <anshumantiwari153@gmail.com>
2026-08-29 12:05:28 +0530

    find maximum sum of subarray by Kadane's Algorithm

---
+++ DSA/Arrays/extraQuestions.java
@@ -0,0 +1,53 @@
+public class extraQuestions {
+    public static void main(String[] args){
+
+        //Find missing number
+
+        // int[] arr = {1,2,3,5,6};
+        // int n = 6;
+
+        // int expectedSum = n*(n+1)/2;
+        // int actualSum = 0;
+
+        // for(int i=0;i<arr.length;i++){
+        //     actualSum += arr[i];
+        // }
+        // int missing = expectedSum - actualSum;
+
+        // System.out.println(missing);
+
+        //Find Maximum Difference
+        
+        // int[] arr = {7,1,5,3,6,4};
+        // int min = arr[0];
+        // int maxDifference = 0;
+        // for(int i=1;i<arr.length;i++){
+        //     int difference = arr[i]-min;
+        //     if(difference > maxDifference){
+        //         maxDifference = difference;
+        //     }
+        //     if(arr[i]<min){
+        //         min=arr[i];
+        //     }
+        //     }
+        // System.out.println(maxDifference);
+
+        //Find maximum sum of subarray(Kadane's Algorithm)
+
+        // int[] arr = {-2,1,-3,4,-1,2,1,-5,4};
+
+        // int currentSum = arr[0];
+        // int maxSum = arr[0];
+
+        // for(int i=1;i<arr.length;i++){
+        //     currentSum = Math.max(arr[i], currentSum + arr[i]);
+        //     maxSum = Math.max(maxSum, currentSum);
+        // }
+        // System.out.println(maxSum);
+
+        //Rotate Array
+
+       
+
+    }
+}
